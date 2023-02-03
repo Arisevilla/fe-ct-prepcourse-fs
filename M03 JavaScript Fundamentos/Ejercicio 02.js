3383361bf5a8abecaa281282195e8a437d9144e4
Arisevilla <arisevilla93@gmail.com>
2023-02-03 12:55:16 -0300

    mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,52 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+  
+   return string
 }
+
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+var resultado = (x + y)
+return resultado
 }
+suma(2,2)
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resultado = (x - y)
+   return resultado
 }
+resta(2,2)
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var resultado = (x/y)
+   return resultado
 }
+divide(10,2)
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var resultado = (x*y)
+   return resultado
 }
+multiplica(2,4)
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var resultado =(x%y)
+   return resultado
 }
+obtenerResto(21,5)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
